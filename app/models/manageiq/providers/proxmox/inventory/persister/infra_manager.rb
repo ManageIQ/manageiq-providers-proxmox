c9a3bce11b2ccdd696afba705d7171289022770f
Adam Grare <adam@grare.com>
2026-02-03 13:51:54 -0500

    Don't reconnect Proxmox VMs
    Proxmox doesn't have a good unique reference that isn't the VM ID which
    can be used to reconnect VMs from storage.
    
    Additionally Proxmox reuses VM IDs so a new VM will get the same ID
    as a previously deleted VM which would then be reconnected.
    
    Fixes https://github.com/ManageIQ/manageiq-providers-proxmox/issues/8

--- app/models/manageiq/providers/proxmox/inventory/persister/infra_manager.rb
+++ app/models/manageiq/providers/proxmox/inventory/persister/infra_manager.rb
@@ -10,6 +10,12 @@ class ManageIQ::Providers::Proxmox::Inventory::Persister::InfraManager < ManageI
     add_collection(infra, :operating_systems, :parent_inventory_collections => %i[vms_and_templates])
     add_collection(infra, :vms_and_templates, {}, {:without_sti => true}) do |builder|
       builder.vm_template_shared
+      # Proxmox doesn't have a good unique reference that isn't the VM ID which
+      # can be used to reconnect VMs from storage.
+      #
+      # Additionally Proxmox reuses VM IDs so a new VM will get the same ID
+      # as a previously deleted VM which would then be reconnected.
+      builder.add_properties(:custom_reconnect_block => nil)
     end
   end
 end
