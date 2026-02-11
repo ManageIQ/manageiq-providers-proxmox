d2a3b577aba33afa9baec3f92adfbae2b2c672fc
stiwa-uejo <253679995+stiwa-uejo@users.noreply.github.com>
2026-02-11 08:59:53 +0100

    Add VM Snapshot

---
+++ app/models/manageiq/providers/proxmox/infra_manager/vm/operations/snapshot.rb
@@ -0,0 +1,134 @@
+module ManageIQ::Providers::Proxmox::InfraManager::Vm::Operations::Snapshot
+  extend ActiveSupport::Concern
+
+  included do
+    supports :snapshots
+    supports :snapshot_create do
+      _("Cannot create snapshot of a template") if template?
+    end
+    supports :remove_snapshot do
+      _("No snapshots available to remove") unless snapshots.count.positive?
+    end
+    supports :revert_to_snapshot do
+      _("No snapshots available to revert to") unless snapshots.count.positive?
+    end
+    supports :remove_all_snapshots do
+      _("No snapshots available to remove") unless snapshots.count.positive?
+    end
+  end
+
+  def params_for_create_snapshot
+    {
+      :fields => [
+        {
+          :component  => 'text-field',
+          :name       => 'name',
+          :id         => 'name',
+          :label      => _('Name'),
+          :isRequired => true,
+          :helperText => _('Must start with a letter and contain only letters, numbers, and underscores.'),
+          :validate   => [
+            {:type => 'required'},
+            {:type => 'pattern', :pattern => '^[a-zA-Z][a-zA-Z0-9_]*$', :message => _('Must start with a letter and contain only letters, numbers, and underscores')}
+          ]
+        },
+        {
+          :component => 'textarea',
+          :name      => 'description',
+          :id        => 'description',
+          :label     => _('Description')
+        },
+        {
+          :component  => 'switch',
+          :name       => 'memory',
+          :id         => 'memory',
+          :label      => _('Snapshot VM memory'),
+          :onText     => _('Yes'),
+          :offText    => _('No'),
+          :isDisabled => current_state != 'on',
+          :helperText => _('Snapshotting the memory is only available if the VM is powered on.')
+        }
+      ]
+    }
+  end
+
+  def raw_create_snapshot(name, desc = nil, memory = false)
+    _log.info("Creating snapshot for VM #{self.name} with name=#{name.inspect}, desc=#{desc.inspect}, memory=#{memory.inspect}")
+    raise MiqException::MiqVmSnapshotError, "Snapshot name is required" if name.blank?
+
+    with_provider_connection do |connection|
+      params = {:snapname => name}
+      params[:description] = desc if desc.present?
+      params[:vmstate] = 1 if memory && current_state == 'on'
+      # Pass as query params to work around SDK bug (body.to_json instead of form-encoding)
+      connection.request(:post, "/nodes/#{host.ems_ref}/qemu/#{ems_ref}/snapshot?#{URI.encode_www_form(params)}")
+    end
+  rescue => err
+    error_message = parse_api_error(err)
+    create_notification(:vm_snapshot_failure, :error => error_message, :snapshot_op => "create")
+    raise MiqException::MiqVmSnapshotError, error_message
+  end
+
+  def raw_remove_snapshot(snapshot_id)
+    snapshot = snapshots.find_by(:id => snapshot_id)
+    raise _("Requested VM snapshot not found, unable to remove snapshot") unless snapshot
+
+    with_provider_connection do |connection|
+      connection.request(:delete, "/nodes/#{host.ems_ref}/qemu/#{ems_ref}/snapshot/#{snapshot.name}")
+    end
+  rescue => err
+    error_message = parse_api_error(err)
+    create_notification(:vm_snapshot_failure, :error => error_message, :snapshot_op => "remove")
+    raise MiqException::MiqVmSnapshotError, error_message
+  end
+
+  def raw_revert_to_snapshot(snapshot_id)
+    raise MiqException::MiqVmError, unsupported_reason(:revert_to_snapshot) unless supports?(:revert_to_snapshot)
+
+    snapshot = snapshots.find_by(:id => snapshot_id)
+    raise _("Requested VM snapshot not found, unable to revert to snapshot") unless snapshot
+
+    with_provider_connection do |connection|
+      connection.request(:post, "/nodes/#{host.ems_ref}/qemu/#{ems_ref}/snapshot/#{snapshot.name}/rollback")
+    end
+  rescue => err
+    error_message = parse_api_error(err)
+    create_notification(:vm_snapshot_failure, :error => error_message, :snapshot_op => "revert")
+    raise MiqException::MiqVmSnapshotError, error_message
+  end
+
+  def raw_remove_all_snapshots
+    raise MiqException::MiqVmError, unsupported_reason(:remove_all_snapshots) unless supports?(:remove_all_snapshots)
+
+    with_provider_connection do |connection|
+      snapshot_list = connection.request(:get, "/nodes/#{host.ems_ref}/qemu/#{ems_ref}/snapshot")
+      snapshot_list.each do |snapshot|
+        next if snapshot["name"] == "current"
+
+        connection.request(:delete, "/nodes/#{host.ems_ref}/qemu/#{ems_ref}/snapshot/#{snapshot["name"]}")
+      end
+    end
+  rescue => err
+    error_message = parse_api_error(err)
+    create_notification(:vm_snapshot_failure, :error => error_message, :snapshot_op => "remove_all")
+    raise MiqException::MiqVmSnapshotError, error_message
+  end
+
+  private
+
+  def parse_api_error(err)
+    msg = err.to_s
+    return msg unless msg.start_with?("ApiError:")
+
+    json_str = msg.sub(/^ApiError:\s*/, "")
+    data = JSON.parse(json_str)
+    parts = []
+    parts << data["message"].strip if data["message"].present?
+    if data["errors"].is_a?(Hash)
+      data["errors"].each { |field, error| parts << "#{field}: #{error.strip}" }
+    end
+    parts.any? ? parts.join(" ") : msg
+  rescue JSON::ParserError
+    msg
+  end
+end
